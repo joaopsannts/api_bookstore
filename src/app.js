2163066bdac107f5824797ec110213b7624781fe
Carlos Wilton <carlosvasconcelos.dev@gmail.com>
2025-08-06 09:55:35 -0300

    feat: Model autor criado

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import { conn } from "./config/sequelize.js";
 
+//Tabelas
+import autorModel from "./models/autorModel.js";
+
 const app = express();
 
 app.use(
@@ -13,7 +16,12 @@ app.use(
 );
 app.use(express.json());
 
-conn.sync()
+conn
+  .sync()
+  .then(() => {
+    console.log("Banco de dados conectado 😎");
+  })
+  .catch((error) => console.log(error));
 
 app.get("/", (request, response) => {
   response.status(200).json({ mensagem: "Olá, Mundo" });
