0436c25020f77852409b3b4287a084fe18e29285
Carlos Wilton <carlosvasconcelos.dev@gmail.com>
2025-09-10 14:40:12 -0300

    feat: upload imagens

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const conn = new Sequelize('bookstore3D', 'root', 'root',{
+export const conn = new Sequelize('bookstore3D', 'root', '123456789',{
     host: 'localhost',
     dialect: "mysql",
     port: 3306
